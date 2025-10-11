9880f81052fba73b629db40878f4210083cfb8dd
Omar Taima <OmarTaima@users.noreply.github.com>
2025-10-11 21:07:37 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,40 +13,45 @@ import ContractPage from "./routes/contracts/page";
 import ReportsPage from "./routes/reports/page";
 
 function App() {
-    const router = createBrowserRouter([
+    const router = createBrowserRouter(
+        [
+            {
+                path: "/",
+                element: <Layout />,
+                children: [
+                    { index: true, element: <DashboardPage /> },
+                    { path: "onboarding", element: <OnboardingPage /> },
+                    { path: "clients", element: <ClientsPage /> },
+                    { path: "clients/:id", element: <ClientDetailPage /> },
+                    { path: "planning", element: <PlanningPage /> },
+                    { path: "campaigns", element: <CampaignsPage /> },
+                    { path: "campaigns/plan/:id", element: <PlanPage /> },
+                    { path: "packages", element: <PackagesPage /> },
+                    { path: "contracts", element: <ContractPage /> },
+                    { path: "reports", element: <ReportsPage /> },
+                ],
+            },
+            {
+                path: "analytics",
+                element: <h1 className="title">Analytics</h1>,
+            },
+            {
+                path: "users",
+                element: <h1 className="title">Users</h1>,
+            },
+            {
+                path: "services",
+                element: <h1 className="title">Services</h1>,
+            },
+            {
+                path: "strategies",
+                element: <h1 className="title">Strategies</h1>,
+            },
+        ],
         {
-            path: "/",
-            element: <Layout />,
-            children: [
-                { index: true, element: <DashboardPage /> },
-                { path: "onboarding", element: <OnboardingPage /> },
-                { path: "clients", element: <ClientsPage /> },
-                { path: "clients/:id", element: <ClientDetailPage /> },
-                { path: "planning", element: <PlanningPage /> },
-                { path: "campaigns", element: <CampaignsPage /> },
-                { path: "campaigns/plan/:id", element: <PlanPage /> },
-                { path: "packages", element: <PackagesPage /> },
-                { path: "contracts", element: <ContractPage /> },
-                { path: "reports", element: <ReportsPage /> },
-            ],
+            basename: "/marktingmangmentdashboard",
         },
-        {
-            path: "analytics",
-            element: <h1 className="title">Analytics</h1>,
-        },
-        {
-            path: "users",
-            element: <h1 className="title">Users</h1>,
-        },
-        {
-            path: "services",
-            element: <h1 className="title">Services</h1>,
-        },
-        {
-            path: "strategies",
-            element: <h1 className="title">Strategies</h1>,
-        },
-    ]);
+    );
     return (
         <ThemeProvider storageKey="theme">
             <RouterProvider router={router}></RouterProvider>
